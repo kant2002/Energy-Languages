cb5bdc7c6ad3df2dd0ad0bc8581af0f692e4db18
Andrii Kurdiumov <kant2002@gmail.com>
2025-08-23 16:32:24 +0500

    Fail if build error failed

--- compile_all.py
+++ compile_all.py
@@ -12,6 +12,7 @@ def file_exists(file_path):
         return os.path.isfile(file_path)
 
 def main():
+  has_error = False
   for root, dirs, files in os.walk(path):
     print('Checking ' + root)
     makefile = os.path.join(root, "Makefile")
@@ -27,6 +28,7 @@ def main():
           err_msg = "%s. Code: %s" % (std_err.strip(), pipes.returncode)
           color_print('[E] Error on ' + root + ': ', color='red', bold=True)
           print(err_msg)
+          has_error = True
         elif len(std_err):
           # return code is 0 (no error), but we may want to
           # do something with the info on std_err
@@ -36,6 +38,8 @@ def main():
           color_print('[OK]', color='green')
       if action == 'measure':
         call(['sleep', '5'])
+  if has_error:
+    sys.exit(1)
 
 if __name__ == '__main__':
   if len(sys.argv) == 2:
