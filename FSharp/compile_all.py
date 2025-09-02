777ec17b1719f3f37212bc255d6078ba2263cb38
Andrii Kurdiumov <kant2002@gmail.com>
2025-09-02 09:30:48 +0500

    Run F# by default

--- FSharp/compile_all.py
+++ FSharp/compile_all.py
@@ -12,8 +12,29 @@ def file_exists(file_path):
         return os.path.isfile(file_path)
 
 def main():
+  has_error = False
   for root, dirs, files in os.walk(path):
+    if "_opam" in root:
+      continue
+    if "node_modules" in root:
+      continue
+    if "/bin" in root:
+      continue
+    if "/obj" in root:
+      continue
+    if "/.source" in root:
+      continue
+    if ".git" in root:
+      continue
+    if ".lua" in root:
+      continue
     print('Checking ' + root)
+    if file_exists(os.path.join(root, ".runignore")):
+      print('Skipping ' + root + ' since it is ignore using .runignore')
+      continue
+    if file_exists(os.path.join(root, "..", ".runignore")):
+      print('Skipping ' + root + ' since it is ignore using parent .runignore')
+      continue
     makefile = os.path.join(root, "Makefile")
     if file_exists(makefile):
       cmd = 'cd ' + root + '; make ' + action
@@ -27,6 +48,7 @@ def main():
           err_msg = "%s. Code: %s" % (std_err.strip(), pipes.returncode)
           color_print('[E] Error on ' + root + ': ', color='red', bold=True)
           print(err_msg)
+          has_error = True
         elif len(std_err):
           # return code is 0 (no error), but we may want to
           # do something with the info on std_err
@@ -36,6 +58,8 @@ def main():
           color_print('[OK]', color='green')
       if action == 'measure':
         call(['sleep', '5'])
+  if has_error:
+    sys.exit(1)
 
 if __name__ == '__main__':
   if len(sys.argv) == 2:
@@ -51,4 +75,3 @@ if __name__ == '__main__':
     action = 'compile'
   
   main()
-    
